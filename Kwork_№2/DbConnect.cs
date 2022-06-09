b8f2f299dcbe3f8559979f7812a58130e7f85206
Артем Лепаев <lepaew228@gmail.com>
2022-06-09 22:38:07 +0300

    q

---
+++ Kwork_№2/DbConnect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwork__2
+{
+    public class DbConnect
+    {
+        public string HostDb { get; set; }
+        public string PortDb { get; set; }
+        public string NameDb { get; set; }
+        public string LoginDb { get; set; }
+        public string PasswordDb { get; set; }
+
+    }
+}
