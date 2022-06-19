b11e374a3ee06b0a8e4b6443d56a0cfd6af82cdc
Артем Лепаев <lepaew228@gmail.com>
2022-06-19 15:16:29 +0300

    ,

---
+++ Kwork_№2/MakeReplaceTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwork__2
+{
+    internal class MakeReplaceTable
+    {
+        public MakeReplaceTable()
+        {
+
+        }
+        public void Make()
+        {
+
+        }
+    }
+}
